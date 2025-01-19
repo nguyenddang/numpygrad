fix broadcasting + test nanogpt

demo/nanogpt/model.py
@@ -18,14 +18,13 @@ class CausualSelfAttention(nn.Module):
         self.n_embd = config.n_embd
         self.dropout = config.dropout
         self.mask = npg.tril(npg.ones((1, 1, config.block_size, config.block_size))).reshape(1, 1, config.block_size, config.block_size)
-        
+    
     def forward(self, x: npg.Tensor):
         B, T, C = x.shape
         q, k, v = self.c_attn(x).split(self.n_embd, dim=-1)
         q = q.reshape(B, T, self.n_head, self.n_embd // self.n_head).transpose(1, 2)
         k = k.reshape(B, T, self.n_head, self.n_embd // self.n_head).transpose(1, 2)
         v = v.reshape(B, T, self.n_head, self.n_embd // self.n_head).transpose(1, 2)
-        
         # attention 
         att = (q @ k.transpose(-2, -1)) * (1.0/ k.shape[-1]**0.5)
         att = att.masked_fill(self.mask[:, :, :T, :T] == 0, -np.inf)
@@ -36,6 +35,34 @@ class CausualSelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         
         return y
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.gelu = nn.GeLU()
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+        
+    def forward(self, x: npg.Tensor):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+    
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd, bias=config.bias)
+        self.attn = CausualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd, bias=config.bias)
+        self.mlp = MLP(config)
+        
+    def forward(self, x: npg.Tensor):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
     
 
         

demo/nanogpt/test/model_torch.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-class LayerNorm(nn.Module):
+class LayerNormTorch(nn.Module):
     """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
 
     def __init__(self, ndim, bias):
@@ -18,7 +18,7 @@ class LayerNorm(nn.Module):
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
 
-class CausalSelfAttention(nn.Module):
+class CausalSelfAttentionTorch(nn.Module):
 
     def __init__(self, config):
         super().__init__()
@@ -67,12 +67,12 @@ class CausalSelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         return y
 
-class MLP(nn.Module):
+class MLPTorch(nn.Module):
 
     def __init__(self, config):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.gelu    = nn.GELU()
+        self.gelu    = nn.GELU(approximate='tanh')
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
@@ -83,14 +83,14 @@ class MLP(nn.Module):
         x = self.dropout(x)
         return x
 
-class Block(nn.Module):
+class BlockTorch(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = CausalSelfAttention(config)
-        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
-        self.mlp = MLP(config)
+        self.ln_1 = LayerNormTorch(config.n_embd, bias=config.bias)
+        self.attn = CausalSelfAttentionTorch(config)
+        self.ln_2 = LayerNormTorch(config.n_embd, bias=config.bias)
+        self.mlp = MLPTorch(config)
 
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))

demo/nanogpt/test/test_nanogpt.py
@@ -1,33 +1,198 @@
-# import npg
-# import torch
-# import numpy as np
-# from demo.nanogpt.model import CausualSelfAttention
-# from model_torch import CausalSelfAttention as CausalSelfAttentionTorch, GPTConfig
+import npg
+import torch
+import numpy as np
+from demo.nanogpt.model import CausualSelfAttention, MLP, Block
+from model_torch import CausalSelfAttentionTorch, GPTConfig, LayerNormTorch, MLPTorch, BlockTorch
 
-# def test_attention():
-#     config = GPTConfig(block_size=4, n_embd=4, n_head=2, n_layer=1, dropout=0.0)
-#     blockt = CausalSelfAttentionTorch(config)
-#     blockn = CausualSelfAttention(config)
+def test_attention():
+    config = GPTConfig(block_size=4, n_embd=4, n_head=2, n_layer=1, dropout=0.0)
+    blockt = CausalSelfAttentionTorch(config)
+    blockn = CausualSelfAttention(config)
     
-#     xt = torch.randn(1, 4, 4)
-#     yt = torch.randint(0, 4, (1, 4), dtype=torch.long)
-#     xn = npg.Tensor(xt.detach().numpy().astype(np.float32))
-#     yn = npg.Tensor(yt.detach().numpy().astype(np.int32))
+    blockn.c_attn.weight = npg.Tensor(blockt.c_attn.weight.detach().numpy().T, requires_grad=True)
+    blockn.c_attn.bias = npg.Tensor(blockt.c_attn.bias.detach().numpy(), requires_grad=True)
+    blockn.c_proj.weight = npg.Tensor(blockt.c_proj.weight.detach().numpy().T, requires_grad=True)
+    blockn.c_proj.bias = npg.Tensor(blockt.c_proj.bias.detach().numpy(), requires_grad=True)
     
-#     logitst = blockt(xt)
-#     losst = torch.nn.functional.cross_entropy(logitst.view(-1, 4), yt.view(-1))
-#     losst.backward()
+    xt = torch.randn(2, 4, 4)
+    yt = torch.randint(0, 4, (2, 4), dtype=torch.long)
+    xn = npg.Tensor(xt.detach().numpy().astype(np.float32))
+    yn = npg.Tensor(yt.detach().numpy().astype(np.int32))
     
-#     logitsn = blockn(xn)
-#     lossn = npg.cross_entropy(logitsn.reshape(-1, 4), yn.reshape(-1))
-#     lossn.backward()
+    logitst = blockt(xt)
+    losst = torch.nn.functional.cross_entropy(logitst.view(-1, 4), yt.view(-1))
+    losst.backward()
     
-#     assert torch.allclose(torch.from_numpy(blockn.c_attn.W.grad), blockt.c_attn.weight.grad.T, rtol=1e-5, atol=1e-6)
-#     assert torch.allclose(torch.from_numpy(blockn.c_attn.b.grad), blockt.c_attn.bias.grad, rtol=1e-5, atol=1e-6)
-#     assert torch.allclose(torch.from_numpy(blockn.c_proj.W.grad), blockt.c_proj.weight.grad.T, rtol=1e-5, atol=1e-6)
-#     assert torch.allclose(torch.from_numpy(blockn.c_proj.b.grad), blockt.c_proj.bias.grad, rtol=1e-5, atol=1e-6)
-#     assert torch.allclose(torch.from_numpy(lossn.data), losst.data, rtol=1e-5, atol=1e-6)
-#     assert torch.allclose(torch.from_numpy(logitsn.data), logitst.data, rtol=1e-5, atol=1e-6)
-    
+    logitsn = blockn(xn)
+    lossn = npg.cross_entropy(logitsn.reshape(-1, 4), yn.reshape(-1))
+    lossn.backward()
+    assert torch.allclose(torch.from_numpy(blockn.c_attn.weight.grad), blockt.c_attn.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.c_attn.bias.grad), blockt.c_attn.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.c_proj.weight.grad), blockt.c_proj.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.c_proj.bias.grad), blockt.c_proj.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lossn.data), losst.data, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(logitsn.data), logitst.data, rtol=1e-5, atol=1e-6)
 
+def test_layernorm():
+    lnt = LayerNormTorch(4, bias=True)
+    lnn = npg.nn.LayerNorm(4, bias=True)
+    mlpt = MLPTorch(GPTConfig(block_size=4, n_embd=4, n_head=2, n_layer=1, dropout=0.0))
+    mlpn = MLP(GPTConfig(block_size=4, n_embd=4, n_head=2, n_layer=1, dropout=0.0))
+    # make sure the weights are the same
+    lnn.weight = npg.Tensor(lnt.weight.detach().numpy(), requires_grad=True)
+    lnn.bias = npg.Tensor(lnt.bias.detach().numpy(), requires_grad=True)
+    mlpn.c_fc.weight = npg.Tensor(mlpt.c_fc.weight.detach().numpy().T, requires_grad=True)
+    mlpn.c_fc.bias = npg.Tensor(mlpt.c_fc.bias.detach().numpy(), requires_grad=True)
+    mlpn.c_proj.weight = npg.Tensor(mlpt.c_proj.weight.detach().numpy().T, requires_grad=True)
+    mlpn.c_proj.bias = npg.Tensor(mlpt.c_proj.bias.detach().numpy(), requires_grad=True)
+    
+    xt = torch.randn(2, 4, 4)
+    target = torch.randint(0, 4, (2, 4), dtype=torch.long)
+    xn = npg.Tensor(xt.detach().numpy().astype(np.float32))
+    yn = npg.Tensor(target.detach().numpy().astype(np.int32))
+    
+    logitst = mlpt(lnt(xt))
+    losst = torch.nn.functional.cross_entropy(logitst.view(-1, 4), target.view(-1))
+    losst.backward()
+    
+    logitsn = mlpn(lnn(xn))
+    lossn = npg.cross_entropy(logitsn.reshape(-1, 4), yn.reshape(-1))
+    lossn.backward()
+    
+    assert torch.allclose(torch.from_numpy(lnn.weight.grad), lnt.weight.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lnn.bias.grad), lnt.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lossn.data), losst.data, rtol=1e-5, atol=1e-6)
+    
+def test_mlp():
+    config = GPTConfig(block_size=4, n_embd=4, n_head=2, n_layer=1, dropout=0.0)
+    mlpn = MLP(config)
+    mlpt = MLPTorch(config)
+    
+    mlpn.c_fc.weight = npg.Tensor(mlpt.c_fc.weight.detach().numpy().T, requires_grad=True)
+    mlpn.c_fc.bias = npg.Tensor(mlpt.c_fc.bias.detach().numpy(), requires_grad=True)
+    mlpn.c_proj.weight = npg.Tensor(mlpt.c_proj.weight.detach().numpy().T, requires_grad=True)
+    mlpn.c_proj.bias = npg.Tensor(mlpt.c_proj.bias.detach().numpy(), requires_grad=True)
+    
+    x = torch.randn(2, 4, 4)
+    target = torch.randint(0, 4, (2, 4), dtype=torch.long)
+    xn = npg.Tensor(x.detach().numpy().astype(np.float32))
+    yn = npg.Tensor(target.detach().numpy().astype(np.int32))
+    
+    logitst = mlpt(x)
+    losst = torch.nn.functional.cross_entropy(logitst.view(-1, 4), target.view(-1))
+    losst.backward()
+    
+    logitsn = mlpn(xn)
+    lossn = npg.cross_entropy(logitsn.reshape(-1, 4), yn.reshape(-1))
+    lossn.backward()
+    
+    assert torch.allclose(torch.from_numpy(mlpn.c_fc.weight.grad), mlpt.c_fc.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(mlpn.c_fc.bias.grad), mlpt.c_fc.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(mlpn.c_proj.weight.grad), mlpt.c_proj.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(mlpn.c_proj.bias.grad), mlpt.c_proj.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lossn.data), losst.data, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(logitsn.data), logitst.data, rtol=1e-5, atol=1e-6)
+    
+def test_block_manual():
+    config = GPTConfig(block_size=4, n_embd=4, n_head=2, n_layer=1, dropout=0.0)
+    lnt1 = LayerNormTorch(4, bias=True)
+    attnt = CausalSelfAttentionTorch(config)
+    lnt2 = LayerNormTorch(4, bias=True)
+    mlpt = MLPTorch(config)
+    
+    lnn1 = npg.nn.LayerNorm(4, bias=True)
+    attnn = CausualSelfAttention(config)
+    lnn2 = npg.nn.LayerNorm(4, bias=True)
+    mlpn = MLP(config)
+    
+    attnn.c_attn.weight = npg.Tensor(attnt.c_attn.weight.detach().numpy().T, requires_grad=True)
+    attnn.c_attn.bias = npg.Tensor(attnt.c_attn.bias.detach().numpy(), requires_grad=True)
+    attnn.c_proj.weight = npg.Tensor(attnt.c_proj.weight.detach().numpy().T, requires_grad=True)
+    attnn.c_proj.bias = npg.Tensor(attnt.c_proj.bias.detach().numpy(), requires_grad=True)
+    lnn1.weight = npg.Tensor(lnt1.weight.detach().numpy(), requires_grad=True)
+    lnn1.bias = npg.Tensor(lnt1.bias.detach().numpy(), requires_grad=True)
+    lnn2.weight = npg.Tensor(lnt2.weight.detach().numpy(), requires_grad=True)
+    lnn2.bias = npg.Tensor(lnt2.bias.detach().numpy(), requires_grad=True)
+    mlpn.c_fc.weight = npg.Tensor(mlpt.c_fc.weight.detach().numpy().T, requires_grad=True)
+    mlpn.c_fc.bias = npg.Tensor(mlpt.c_fc.bias.detach().numpy(), requires_grad=True)
+    mlpn.c_proj.weight = npg.Tensor(mlpt.c_proj.weight.detach().numpy().T, requires_grad=True)
+    mlpn.c_proj.bias = npg.Tensor(mlpt.c_proj.bias.detach().numpy(), requires_grad=True)
+    
+    x = torch.randn(2, 4, 4)
+    target = torch.randint(0, 4, (2, 4), dtype=torch.long)
+    xn = npg.Tensor(x.detach().numpy().astype(np.float32))
+    yn = npg.Tensor(target.detach().numpy().astype(np.int32))
+    logitst = mlpt(lnt2(x + attnt(lnt1(x))))
+    losst = torch.nn.functional.cross_entropy(logitst.view(-1, 4), target.view(-1))
+    losst.backward()
+    
+    logitsn = mlpn(lnn2(xn + attnn(lnn1(xn))))
+    lossn = npg.cross_entropy(logitsn.reshape(-1, 4), yn.reshape(-1))
+    lossn.backward()
+    
+    assert torch.allclose(torch.from_numpy(attnn.c_attn.weight.grad), attnt.c_attn.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(attnn.c_attn.bias.grad), attnt.c_attn.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(attnn.c_proj.weight.grad), attnt.c_proj.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(attnn.c_proj.bias.grad), attnt.c_proj.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lnn1.weight.grad), lnt1.weight.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lnn1.bias.grad), lnt1.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lnn2.weight.grad), lnt2.weight.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lnn2.bias.grad), lnt2.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(mlpn.c_fc.weight.grad), mlpt.c_fc.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(mlpn.c_fc.bias.grad), mlpt.c_fc.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(mlpn.c_proj.weight.grad), mlpt.c_proj.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(mlpn.c_proj.bias.grad), mlpt.c_proj.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lossn.data), losst.data, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(logitsn.data), logitst.data, rtol=1e-5, atol=1e-6)
+    
+class test_block():
+    config = GPTConfig(block_size=4, n_embd=4, n_head=2, n_layer=1, dropout=0.0)
+    blockt = BlockTorch(config)
+    blockn = Block(config)
+    
+    blockn.attn.c_attn.weight = npg.Tensor(blockt.attn.c_attn.weight.detach().numpy().T, requires_grad=True)
+    blockn.attn.c_attn.bias = npg.Tensor(blockt.attn.c_attn.bias.detach().numpy(), requires_grad=True)
+    blockn.attn.c_proj.weight = npg.Tensor(blockt.attn.c_proj.weight.detach().numpy().T, requires_grad=True)
+    blockn.attn.c_proj.bias = npg.Tensor(blockt.attn.c_proj.bias.detach().numpy(), requires_grad=True)
+    blockn.ln_1.weight = npg.Tensor(blockt.ln_1.weight.detach().numpy(), requires_grad=True)
+    blockn.ln_1.bias = npg.Tensor(blockt.ln_1.bias.detach().numpy(), requires_grad=True)
+    blockn.ln_2.weight = npg.Tensor(blockt.ln_2.weight.detach().numpy(), requires_grad=True)
+    blockn.ln_2.bias = npg.Tensor(blockt.ln_2.bias.detach().numpy(), requires_grad=True)
+    blockn.mlp.c_fc.weight = npg.Tensor(blockt.mlp.c_fc.weight.detach().numpy().T, requires_grad=True)
+    blockn.mlp.c_fc.bias = npg.Tensor(blockt.mlp.c_fc.bias.detach().numpy(), requires_grad=True)
+    blockn.mlp.c_proj.weight = npg.Tensor(blockt.mlp.c_proj.weight.detach().numpy().T, requires_grad=True)
+    blockn.mlp.c_proj.bias = npg.Tensor(blockt.mlp.c_proj.bias.detach().numpy(), requires_grad=True)
+    
+    xt = torch.randn(2, 4, 4)
+    target = torch.randint(0, 4, (2, 4), dtype=torch.long)
+    xn = npg.Tensor(xt.detach().numpy().astype(np.float32))
+    yn = npg.Tensor(target.detach().numpy().astype(np.int32))
+    
+    logitst = blockt(xt)
+    losst = torch.nn.functional.cross_entropy(logitst.view(-1, 4), target.view(-1))
+    losst.backward()
+    
+    logitsn = blockn(xn)
+    lossn = npg.cross_entropy(logitsn.reshape(-1, 4), yn.reshape(-1))
+    lossn.backward()
+    
+    assert torch.allclose(torch.from_numpy(blockn.attn.c_attn.weight.grad), blockt.attn.c_attn.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.attn.c_attn.bias.grad), blockt.attn.c_attn.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.attn.c_proj.weight.grad), blockt.attn.c_proj.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.attn.c_proj.bias.grad), blockt.attn.c_proj.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.ln_1.weight.grad), blockt.ln_1.weight.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.ln_1.bias.grad), blockt.ln_1.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.ln_2.weight.grad), blockt.ln_2.weight.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.ln_2.bias.grad), blockt.ln_2.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.mlp.c_fc.weight.grad), blockt.mlp.c_fc.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.mlp.c_fc.bias.grad), blockt.mlp.c_fc.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.mlp.c_proj.weight.grad), blockt.mlp.c_proj.weight.grad.T, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(blockn.mlp.c_proj.bias.grad), blockt.mlp.c_proj.bias.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(lossn.data), losst.data, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(logitsn.data), logitst.data, rtol=1e-5, atol=1e-6)
+    
+    
+    
+    
+    
     

npg/engine.py
@@ -28,47 +28,49 @@ class Tensor:
         # find axes that need to be broadcasted
         broadcasted_axes_self = tuple(i for i, (a, c) in enumerate(zip(expanded_self_shape, out.shape)) if a == 1 and c > 1)
         broadcasted_axes_other = tuple(i for i, (b, c) in enumerate(zip(expanded_other_shape, out.shape)) if b == 1 and c > 1)
-        return broadcasted_axes_self, broadcasted_axes_other
+        keepdim_self = (len(self.shape) == len(other.shape)) and (len(broadcasted_axes_self)) > 0
+        keepdim_other = (len(self.shape) == len(other.shape)) and (len(broadcasted_axes_other)) > 0
+        return broadcasted_axes_self, broadcasted_axes_other, keepdim_self, keepdim_other
     
     def __add__(self, other):
         other, out_requires_grad = self.check_op(other)
         out = Tensor(self.data + other.data, _children=(self, other), grad_fn='AddBackward', requires_grad=out_requires_grad)
-        broadcasted_axes_self, broadcasted_axes_other = self.check_broadcast(other, out)
+        broadcasted_axes_self, broadcasted_axes_other, keepdim_self, keepdim_other = self.check_broadcast(other, out)
         def _backward():
             if self.requires_grad:
                 sgrad = 1.0 * out.grad
-                self.grad += np.sum(sgrad, axis=broadcasted_axes_self) if broadcasted_axes_self else sgrad
+                self.grad += np.sum(sgrad, axis=broadcasted_axes_self, keepdims=keepdim_self) if broadcasted_axes_self else sgrad
             if other.requires_grad:
                 ograd = 1.0 * out.grad
-                other.grad += np.sum(ograd, axis=broadcasted_axes_other) if broadcasted_axes_other else ograd
+                other.grad += np.sum(ograd, axis=broadcasted_axes_other, keepdims=keepdim_other) if broadcasted_axes_other else ograd
         out._backward = _backward
         return out
     
     def __mul__(self, other):
         other, out_requires_grad = self.check_op(other)
         out = Tensor(self.data * other.data, _children=(self, other), grad_fn='MulBackward', requires_grad=out_requires_grad)
-        broadcasted_axes_self, broadcasted_axes_other = self.check_broadcast(other, out)
+        broadcasted_axes_self, broadcasted_axes_other, keepdim_self, keepdim_other = self.check_broadcast(other, out)
         def _backward():
             if self.requires_grad:
                 sgrad = other.data * out.grad
-                self.grad += np.sum(sgrad, axis=broadcasted_axes_self) if broadcasted_axes_self else sgrad
+                self.grad += np.sum(sgrad, axis=broadcasted_axes_self, keepdims=keepdim_self) if broadcasted_axes_self else sgrad
             if other.requires_grad:
                 ograd = self.data * out.grad
-                other.grad += np.sum(ograd, axis=broadcasted_axes_other) if broadcasted_axes_other else ograd
+                other.grad += np.sum(ograd, axis=broadcasted_axes_other, keepdims=keepdim_other) if broadcasted_axes_other else ograd
         out._backward = _backward
         return out
     
     def __matmul__(self, other):
         other, out_requires_grad = self.check_op(other)
         out =  Tensor(self.data @ other.data, _children=(self, other), grad_fn='MatmulBackward', requires_grad=out_requires_grad)
-        broadcasted_axes_self, broadcasted_axes_other = self.check_broadcast(other, out)
+        broadcasted_axes_self, broadcasted_axes_other, keepdim_self, keepdim_other = self.check_broadcast(other, out)
         def _backward():
             if self.requires_grad:
                 sgrad = out.grad @ np.swapaxes(other.data, -1, -2)
-                self.grad += np.sum(sgrad, axis=broadcasted_axes_self) if broadcasted_axes_self else sgrad
+                self.grad += np.sum(sgrad, axis=broadcasted_axes_self, keepdims=keepdim_self) if broadcasted_axes_self else sgrad
             if other.requires_grad:
                 ograd = np.swapaxes(self.data, -1, -2) @ out.grad
-                other.grad += np.sum(ograd, axis=broadcasted_axes_other) if broadcasted_axes_other else ograd
+                other.grad += np.sum(ograd, axis=broadcasted_axes_other, keepdims=keepdim_other) if broadcasted_axes_other else ograd
         out._backward = _backward
         return out
     

npg/functional.py
@@ -110,11 +110,20 @@ def softmax(x: npg.Tensor, dim: int = -1) -> npg.Tensor:
     return out
  
 # loss functions
-def cross_entropy(logits: npg.Tensor, target: npg.Tensor) -> npg.Tensor:
-    prob = npg.softmax(logits, dim=-1)
-    class_prob = prob[np.arange(prob.shape[0]), target.data]
-    loss = -npg.log(class_prob)
-    return npg.mean(loss)
+def cross_entropy(x: npg.Tensor, target: npg.Tensor) -> npg.Tensor:
+    max_vals = np.max(x.data, axis=-1, keepdims=True)  # shift for stability
+    exp_data = np.exp(x.data - max_vals)
+    probs = exp_data / np.sum(exp_data, axis=-1, keepdims=True)
+    loss = -np.log(probs[np.arange(x.data.shape[0]), target.data]).mean()
+    out = npg.Tensor(np.array(loss), _children=(x, target), grad_fn='CrossEntropyBackward', requires_grad=x.requires_grad)
+    
+    def _backward():
+        if x.requires_grad:
+            softmax_x = probs
+            softmax_x[np.arange(x.data.shape[0]), target.data] -= 1
+            x.grad += softmax_x * out.grad / x.data.shape[0]
+    out._backward = _backward
+    return out
 
 def mse_loss(pred: npg.Tensor, target: npg.Tensor) -> npg.Tensor:
     return npg.mean((pred - target)**2)

npg/nn.py
@@ -23,17 +23,17 @@ class Linear(Module):
     
     def __init__(self, in_features, out_features, bias=True):
         super().__init__()
-        self.W = npg.randn(in_features, out_features, requires_grad=True)
-        self.b = npg.randn(out_features, requires_grad=True) if bias else None
+        self.weight = npg.randn(in_features, out_features, requires_grad=True)
+        self.bias = npg.randn(out_features, requires_grad=True) if bias else None
         
     def forward(self, x):
-        return x @ self.W + self.b
+        return x @ self.weight + self.bias
     
     def parameters(self):
-        return [self.W, self.b] if self.b is not None else [self.W]
+        return [self.weight, self.bias] if self.bias is not None else [self.weight]
     
     def __repr__(self):
-        return f"npg.nn.Linear({self.W.data.shape[0]}, {self.W.data.shape[1]})"
+        return f"npg.nn.Linear({self.weight.data.shape[0]}, {self.weight.data.shape[1]})"
 
 # Activation functions
 class ReLU(Module):
@@ -81,24 +81,27 @@ class GeLU(Module):
         return "npg.nn.GeLU()"
     
 # Normalization
-class Layernorm(Module):
+class LayerNorm(Module):
     
-    def __init__(self, normalized_shape, eps=1e-5, bias=True):
+    def __init__(self, ndim, eps=1e-5, bias=True):
         super().__init__()
-        self.weight = npg.ones(normalized_shape, requires_grad=True)
-        self.bias = npg.zeros(normalized_shape, requires_grad=True) if bias else None
+        self.weight = npg.ones(ndim, requires_grad=True)
+        self.bias = npg.zeros(ndim, requires_grad=True) if bias else None
         self.eps = eps
     
     def forward(self, x):
-        xmean = npg.mean(x, dim=-1, keepdims=True)
-        xvar = npg.var(x, dim=-1, keepdims=True)
-        xhat = (x - xmean) / npg.sqrt(xvar + self.eps)
-        out = xhat * self.weight + self.bias
+        mean = npg.mean(x, dim=-1, keepdim=True)
+        var = npg.var(x, dim=-1, keepdim=True)
+        norm = (x - mean) / npg.sqrt(var + self.eps)
+        out = norm * self.weight + self.bias
         return out
     
     def parameters(self):
-        return [self.weight, self.bias] if self.beta is not None else [self.weight]
+        return [self.weight, self.bias] if self.bias is not None else [self.weight]
 
+    def __repr__(self):
+        return f"npg.nn.LayerNorm({self.weight.data.shape[0]})"
+    
 # Regularisation
 class Dropout(Module):
     def __init__(self, p=0.5):
@@ -111,6 +114,9 @@ class Dropout(Module):
             mask = (npg.rand(*x.data.shape) > self.p)
             return x * mask / (1 - self.p)
         return x
+    
+    def __repr__(self):
+        return f"npg.nn.Dropout(p={self.p})"
 
 # Loss functions
         

test/test_functional.py
@@ -9,13 +9,16 @@ np.random.seed(42)
 
 def test_mean():
     ta = torch.randn(3, 4, requires_grad=True)
-    tc = ta.mean(dim=1)
+    tb = torch.randn(3, 4, requires_grad=True)
+    tc = tb - ta.mean(dim=1, keepdim=True)
     tc.backward(torch.ones_like(tc))
     
     a = npg.Tensor(ta.detach().numpy().astype(np.float32), requires_grad=True)
-    c = npg.mean(a, dim=1)
+    b = npg.Tensor(tb.detach().numpy().astype(np.float32), requires_grad=True)
+    c = b - npg.mean(a, dim=1, keepdim=True)
     c.backward()
     assert torch.allclose(torch.from_numpy(a.grad), ta.grad, rtol=1e-5, atol=1e-6)
+    assert torch.allclose(torch.from_numpy(b.grad), tb.grad, rtol=1e-5, atol=1e-6)
     assert torch.allclose(torch.from_numpy(c.data), tc.data, rtol=1e-5, atol=1e-6)
     
     ta = torch.randn(10,3,7, requires_grad=True)